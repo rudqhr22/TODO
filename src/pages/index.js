09dee7744f9291ee2ae4723ba9da9355c02e3a24
rudqhr22 <rudqhr22@frigatebird.net>
2020-05-19 21:24:41 +0900

    Material-UI 추가 및 테스트

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,21 @@
 import React from "react"
+import { Button } from '@material-ui/core';
 
-export default () => <div> Hello world! </div>
+export default () => {
+
+    return(
+    <div> 
+        <Button variant="contained">
+            Default
+        </Button>
+        <Button variant="contained" color="primary">
+            Primary
+        </Button>
+        <Button>
+            Hello world!         
+        </Button>
+    </div>
+    );
+}
+
 
